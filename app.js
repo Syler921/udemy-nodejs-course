e4883f7ff184d2911fa3621cbc9b9c615217d807
syler921 <syler921@gmail.com>
2020-12-03 00:08:01 +0200

    fixed cart visualziation - next step deletion of cart items

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('5fc7fbfa49160f76707f85b3')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
